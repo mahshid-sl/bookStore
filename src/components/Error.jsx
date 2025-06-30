27d794f7605b5fb77057a87f7c5141d1661489f2
mahshid-sl <mahshiid.soleiman@gmail.com>
2025-06-30 21:25:09 +0330

    feat: add Error and Loading components for improved user feedback

---
+++ src/components/Error.jsx
@@ -0,0 +1,5 @@
+function Error({ children }) {
+  return <div className="text-red-500 text-center py-12">{children}</div>;
+}
+
+export default Error;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import { BeatLoader } from "react-spinners";
+
+function Loading() {
+  return (
+    <div className="flex items-center justify-center py-12">
+      <BeatLoader color="#FB9E22" />
+    </div>
+  );
+}
+
+export default Loading;
