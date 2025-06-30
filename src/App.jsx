164b6229a3fde620843a002e4c42e96acbfa50af
mahshid-sl <mahshiid.soleiman@gmail.com>
2025-06-30 23:49:00 +0330

    build contactUs - Cart pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import HomePage from "./pages/HomePage";
 import PopularBookPage from "./pages/PopularBookPage";
 import { Book } from "lucide-react";
 import BookDetails from "./pages/BookDetails";
+import ContactUs from "./pages/ContactUs";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <div dir="rtl" className="app bg-white font-vazirNormal ">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="contact" element={<ContactUs />} />
+          <Route path="cart" element={<Cart />} />
           <Route path="popularbook" element={<PopularBookPage />} />
           <Route path="details" element={<BookDetails />} />
         </Routes>
---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,5 @@
+function ContactUs() {
+  return <div>contact us</div>;
+}
+
+export default ContactUs;
